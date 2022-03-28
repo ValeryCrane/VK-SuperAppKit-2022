476bef75648348bc75c816aea6395a9474c63af3
Valery Crane <zhulery@gmail.com>
2022-03-28 17:55:28 +0500

    Created UITableView with 10 empty cells

--- Black-Matter-Detector/InfoScene/InfoViewController.swift
+++ Black-Matter-Detector/InfoScene/InfoViewController.swift
@@ -8,10 +8,40 @@
 import UIKit
 
 class InfoViewController: UIViewController {
+    private let tableView = UITableView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+        configureTableView()
+    }
+    
+    private func configureTableView() {
+        view.addSubview(tableView)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor)
+        ])
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 
+}
+
+// MARK: - UITableViewDelegate & DataSource implementation.
+extension InfoViewController: UITableViewDelegate { }
+
+extension InfoViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        UITableViewCell()
+    }
+    
+    
 }
