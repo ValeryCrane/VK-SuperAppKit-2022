81342587c8e6174f41e87eb03cca13c555ea9b94
Valery Crane <zhulery@gmail.com>
2022-03-28 17:37:36 +0500

    Initial blank project

---
+++ Black-Matter-Detector/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Black-Matter-Detector
+//
+//  Created by Валерий Журавлев on 28.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
